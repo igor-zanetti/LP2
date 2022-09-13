ebda08ab47b1354854531791cb0da75b821e8493
igor-zanetti <110908223+igor-zanetti@users.noreply.github.com>
2022-09-13 11:03:33 -0300

    Add files via upload

---
+++ Atividade2/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSomar = new System.Windows.Forms.Button();
+            this.btnSubtrair = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.lblnum1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.lblnum2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSomar
+            // 
+            this.btnSomar.Location = new System.Drawing.Point(41, 281);
+            this.btnSomar.Name = "btnSomar";
+            this.btnSomar.Size = new System.Drawing.Size(75, 54);
+            this.btnSomar.TabIndex = 0;
+            this.btnSomar.Text = "+";
+            this.btnSomar.UseVisualStyleBackColor = true;
+            this.btnSomar.Click += new System.EventHandler(this.btnSomar_Click);
+            // 
+            // btnSubtrair
+            // 
+            this.btnSubtrair.Location = new System.Drawing.Point(131, 281);
+            this.btnSubtrair.Name = "btnSubtrair";
+            this.btnSubtrair.Size = new System.Drawing.Size(75, 54);
+            this.btnSubtrair.TabIndex = 1;
+            this.btnSubtrair.Text = "-";
+            this.btnSubtrair.UseVisualStyleBackColor = true;
+            this.btnSubtrair.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Location = new System.Drawing.Point(222, 281);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(75, 54);
+            this.btnMultiplicar.TabIndex = 2;
+            this.btnMultiplicar.Text = "*";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.Location = new System.Drawing.Point(314, 281);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(75, 54);
+            this.btnDividir.TabIndex = 3;
+            this.btnDividir.Text = "/";
+            this.btnDividir.UseVisualStyleBackColor = true;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
+            // 
+            // lblnum1
+            // 
+            this.lblnum1.AutoSize = true;
+            this.lblnum1.Location = new System.Drawing.Point(38, 64);
+            this.lblnum1.Name = "lblnum1";
+            this.lblnum1.Size = new System.Drawing.Size(53, 13);
+            this.lblnum1.TabIndex = 4;
+            this.lblnum1.Text = "Número 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 5;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(38, 124);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(55, 13);
+            this.lblResultado.TabIndex = 6;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Location = new System.Drawing.Point(106, 61);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero1.TabIndex = 7;
+            this.txtNumero1.TextChanged += new System.EventHandler(this.txtNumero1_TextChanged);
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Location = new System.Drawing.Point(106, 90);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero2.TabIndex = 8;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(106, 121);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(100, 20);
+            this.txtResultado.TabIndex = 9;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(482, 64);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 57);
+            this.btnLimpar.TabIndex = 10;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(535, 381);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 57);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Sair";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // lblnum2
+            // 
+            this.lblnum2.AutoSize = true;
+            this.lblnum2.Location = new System.Drawing.Point(38, 97);
+            this.lblnum2.Name = "lblnum2";
+            this.lblnum2.Size = new System.Drawing.Size(53, 13);
+            this.lblnum2.TabIndex = 12;
+            this.lblnum2.Text = "Número 2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 450);
+            this.Controls.Add(this.lblnum2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.txtNumero1);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblnum1);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnSubtrair);
+            this.Controls.Add(this.btnSomar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSomar;
+        private System.Windows.Forms.Button btnSubtrair;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.Label lblnum1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label lblnum2;
+    }
+}
+
---
+++ Atividade2/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int numero1, numero2, resultado;
+
+            numero1 = Convert.ToInt32(txtNumero1.Text);
+            numero2 = Convert.ToInt32(txtNumero2.Text);
+
+            resultado = numero1 / numero2;
+
+            txtResultado.Text = resultado.ToString();
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtNumero1.Clear();
+            txtNumero2.Clear();
+            txtResultado.Clear();
+
+            txtNumero1.Focus();
+        }
+
+        private void btnSomar_Click(object sender, EventArgs e)
+        {
+            int numero1, numero2, resultado;
+
+            numero1 = Convert.ToInt32(txtNumero1.Text);
+            numero2 = Convert.ToInt32(txtNumero2.Text);
+
+            resultado = numero1 + numero2;
+
+            txtResultado.Text = resultado.ToString();
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            int numero1, numero2, resultado;
+
+            numero1 = Convert.ToInt32(txtNumero1.Text);
+            numero2 = Convert.ToInt32(txtNumero2.Text);
+
+            resultado = numero1 * numero2;
+
+            txtResultado.Text = resultado.ToString();
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            int numero1, numero2, resultado;
+
+            numero1 = Convert.ToInt32(txtNumero1.Text);
+            numero2 = Convert.ToInt32(txtNumero2.Text);
+
+            resultado = numero1 / numero2;
+
+            txtResultado.Text = resultado.ToString();
+
+        }
+
+        private void txtNumero1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
